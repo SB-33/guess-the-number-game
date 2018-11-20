93e917908d5a001f7ce9a9b0d54e20f4fcf9aad1
Maciej <carnageepl@gmail.com>
2018-11-20 01:55:59 +0100

    added decoupledTemplateLogic

---
+++ web/src/main/java/com/sexybeast/utils/thymeleaf/DecoupledLogicSetup.java
@@ -0,0 +1,27 @@
+package com.sexybeast.utils.thymeleaf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Component
+public class DecoupledLogicSetup {
+    // == fields ==
+    private final SpringResourceTemplateResolver templateResolver;
+
+    // == constructors ==
+    public DecoupledLogicSetup(SpringResourceTemplateResolver templateResolver) {
+        this.templateResolver = templateResolver;
+    }
+
+    // == init ==
+    @PostConstruct
+    private void onStart() {
+        templateResolver.setUseDecoupledLogic(true);
+        log.info("Decoupled template logic enabled");
+    }
+
+}
